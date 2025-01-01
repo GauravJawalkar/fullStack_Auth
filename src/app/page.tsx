df63c58ecb6214070654a95ae9f1f6d930e072da
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-01-01 20:38:04 +0530

    final pushhhh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { NextRequest } from "next/server";
 
-export default function Home(request: NextRequest) {
+export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -109,7 +108,5 @@ export default function Home(request: NextRequest) {
     </div >
   );
 }
-function elseIf() {
-  throw new Error("Function not implemented.");
-}
+
 
